54b31f2d2cdd7e714ec1608cf371b34f5c3b0ae8
Rodrigo Manzanares <44766514+rmc1001@users.noreply.github.com>
2020-06-14 11:39:21 +0200

    Create IJuego.java

---
+++ IJuego.java
@@ -0,0 +1,10 @@
+package simulador_juego.enunciado;
+/**
+ * interfaz del juego
+ * @author rodrigomanzanarescelis
+ *
+ */
+public interface IJuego {
+	public void generarEnemigo(int tipoEnemigo);
+	public void eliminarEnemigo(int tipoEnemigo);
+}
